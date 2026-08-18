bbde515e5dc911aab57bac58d0860ac40da5c7c9
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-08-18 13:14:13 +0600

    employee controller working

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get,Param } from '@nestjs/common';
+import { EmployeeService } from './employee.service';
 
 @Controller('employee')
-export class EmployeeController {}
+export class EmployeeController {
+
+    constructor(private readonly employeeService: EmployeeService) {}
+    @Get()
+    getAllEmployees() {
+        return this.employeeService.getAllEmployees();
+    }
+
+    @Get(':id')
+    getEmployeeById(@Param('id') id: string) {
+        return this.employeeService.getEmployeeById(parseInt(id));
+    }
+}
