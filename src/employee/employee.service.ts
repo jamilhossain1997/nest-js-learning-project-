9133649608dc7e4a7501f80dd3975f5a60eb4755
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-08-18 13:07:48 +0600

    employee service working

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+    private employees = [
+        { id: 1, name: 'John Doe', position: 'Software Engineer' },
+        { id: 2, name: 'Jane Smith', position: 'Product Manager' },
+        { id: 3, name: 'Alice Johnson', position: 'UX Designer' },
+    ];
+
+
+    getAllEmployees(){
+        return this.employees;
+    }
+    
+    getEmployeeById(id:number){
+        return this.employees.find(employee =>employee.id === id);
+    }
+}
