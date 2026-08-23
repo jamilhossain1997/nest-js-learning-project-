d2655965f2293a511da5c1a40ad7a9bb36791759
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-08-23 18:33:02 +0600

    Student service working now

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    private students =[
+        { id: 1, name: 'John Doe', age: 20 },
+        { id: 2, name: 'Jane Smith', age: 22 },
+        { id: 3, name: 'Michael Johnson', age: 19 },
+    ];
+
+    getAllStudents(){
+        return this.students;
+    }
+
+    getStudentById(id:number){
+        return this.students.find(student => student.id === id);
+    }
+
+    //POST method to add a new student
+    addStudent(student:{ name:string, age:number}){
+        const NewStudent = {
+            id: Date.now(),
+            ...student
+        }
+        this.students.push(NewStudent);
+        return NewStudent;
+    }
+}
