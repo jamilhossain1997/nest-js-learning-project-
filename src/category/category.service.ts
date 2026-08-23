bc6c9eb58fc661f3573683aeb4a3525a9e3ac1de
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-08-23 17:53:40 +0600

    category service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    getAllCategories() {
+        return [
+            'Electronics',
+            'Books',
+            'Clothing',
+        ];
+    }
+}
